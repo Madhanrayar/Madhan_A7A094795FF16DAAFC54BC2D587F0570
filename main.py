a18daed73e04e578df03f933b64a8aa6535b27b3
Madhanrayar <144341930+Madhanrayar@users.noreply.github.com>
2023-09-08 17:23:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+t1=(1,2)
+print(type (t1))
+print (type(l1))
+l1=["maruthi","madhendira","aadi",
+"beny","honda"]
+print(l1)
+for i in ln():
+if i == l1
+print i.upper()
+else
+print i.title()
+dict1={}
+dict2={"name":"abc"}
+print(dict 2 get("name","invalid keyword"))
